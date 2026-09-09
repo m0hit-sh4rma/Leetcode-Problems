db3ec7aeb7a2644a787c470375765bc7a138159e
Mohit Sharma <mohitsharma41444@gmail.com>
2026-09-09 19:01:32 +0530

    Time: 739 ms (7.01%) | Memory: 47.2 MB (27.68%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,19 +1,25 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> list = new ArrayList<>();
-        char[] arr = p.toCharArray();
-        Arrays.sort(arr);
-        int n = arr.length;
-        p = new String(arr);
+        int i = 0;
+        int j = p.length();
 
-        for (int i = 0; i < s.length() - n + 1; i++) {
-            String sub = s.substring(i, i + n);
-            char[] chars = sub.toCharArray();
-            Arrays.sort(chars);
-            sub = new String(chars);
-
-            if (p.equals(sub)) list.add(i);
+        while (j <= s.length()) {
+            if (isAnagram(s.substring(i, j), p)) list.add(i);
+            i++;
+            j++;
         }
         return list;
+    }
+    private boolean isAnagram(String s, String t) {
+        int[] frq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            frq[s.charAt(i) - 'a']++;
+            frq[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : frq) if (count != 0) return false;
+        return true;
     }
 }
