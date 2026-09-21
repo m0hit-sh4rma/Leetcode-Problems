f9521b7a160409d19df388070da537e196aac036
Mohit Sharma <mohitsharma41444@gmail.com>
2026-09-21 13:50:16 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (18.32%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -3,9 +3,7 @@ class Solution {
         int degree = 0;
         int i = 1;
         for (char ch : s.toCharArray()) {
-            int rev = 26 - (ch - 'a');
-            degree += (rev * i);
-            i++;
+            degree += ((26 - (ch - 'a')) * (i++));
         }
         return degree;
     }
